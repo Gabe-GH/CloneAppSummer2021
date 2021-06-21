b2063a86568902431cf7c4da20c04fd6f792db8c
Gabe-GH <gabriel.lira01@utrgv.edu>
2021-06-21 00:12:36 -0500

    connected mongodb with a config file to hide credentials and a gitignore to ignore pushing config file to github

---
+++ Mongo/index.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require('mongodb');
+//const uri = require("./config.js");
+
+const uri = 'mongodb+srv://classed:r945XJMmtne9BzL7@utrgvrmp.zsqqp.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+
+async function main() {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+    } catch(e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases: ");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
